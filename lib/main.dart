7a643d62881164bf9a5c8f8cef8ce18af5b082cf
ANGEL_MIGUEL_VALENCIA_CARRILLO <a.22308051280738@cbtis128.edu.mx>
2025-03-21 02:06:40 +0000

    Act2

--- lib/main.dart
+++ lib/main.dart
@@ -3,39 +3,55 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Angel Valencia 0738'),
+          backgroundColor: Colors.blue, // Color del AppBar
+        ),
+        body: Center(
+          child: Container(
+            padding: EdgeInsets.all(20),
+            decoration: BoxDecoration(
+              color: Colors.amber[100], // Color de fondo del Container
+              borderRadius: BorderRadius.circular(20), // Bordes redondeados
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black26,
+                  blurRadius: 10,
+                  offset: Offset(0, 4), // Sombra
+                ),
+              ],
+            ),
+            child: Column(
+              mainAxisSize: MainAxisSize.min, // Ajusta el tamaño de la columna
+              children: [
+                Text('Nombre: Angel',
+                    style: TextStyle(
+                      fontSize: 18, // Tamaño de letra
+                      fontFamily: 'Roboto', // Tipo de letra
+                      color: Colors.blue[900], // Color de letra
+                    )),
+                SizedBox(height: 10), // Espacio vertical entre textos
+                Text('Apellido: Valencia',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontFamily: 'Roboto',
+                      color: Color(0xff1ea10d),
+                    )),
+                SizedBox(height: 10), // Espacio vertical entre textos
+                Text('Edad: 25',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontFamily: 'Roboto',
+                      color: Color(0xffa1230d),
+                    )),
+              ],
+            ),
+          ),
         ),
       ),
     );
